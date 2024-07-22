24011e533b473dc6c65be11efeb744c25dec1f78
Martin Claus <martin.claus@iits-consulting.de>
2024-07-22 15:32:43 +0200

    Initial Commit

---
+++ IDnow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IDnow
+//
+//  Created by Martin Claus on 22.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IDnow/IDnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDnowApp.swift
+//  IDnow
+//
+//  Created by Martin Claus on 22.07.24.
+//
+
+import SwiftUI
+
+@main
+struct IDnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
